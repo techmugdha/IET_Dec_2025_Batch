41a5cc21bbfb81eac95e53e7da2eebb934130ea6
Tech Mugdha <the.tech.mugdha@gmail.com>
2025-12-19 13:09:25 +0530

    Day 05: Observer Pattern,  array [], Non- Generic Collections - ArrayList, HashTable,  is and as keywords, Generic Concept, Geneic Classes and Generic MEthods, PAss by value and Pass by refernce, params and out keywords, Tuples, Dynamic Type, Foreach loop,  Generic Collection - List<T>, Dictionary<TKey, TValue>

---
+++ CSharpDemos/21Demo_ObserverPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _21Demo_ObserverPattern
+{
+    public delegate void NotifyEventHandler(string message);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher publisher = new Publisher();
+            Subscriber subscriber = new Subscriber();
+
+            // coupling multiple methods to the event :: Multi-cast event
+            publisher.Notify += subscriber.MethodA;
+            publisher.Notify += subscriber.MethodB;
+
+            // Raise the event
+            publisher.NotifySubscribers("The Grand Sale is UP! Upto 70% off on selected products!");
+
+            // optout MethodB from receiving notifications
+            publisher.Notify -= subscriber.MethodB;
+            // Raise the event again
+            publisher.NotifySubscribers("Flash Sale! Extra 10% off on Electronics!");
+        }
+    }
+    public class Subscriber
+    {
+        public void MethodA(string message)
+        {
+            Console.WriteLine($"MethodA received: {message} via SMS");
+        }
+        public void MethodB(string message)
+        {
+            Console.WriteLine($"MethodB received: {message} via EMail");
+        }
+    }
+
+    public class Publisher
+    {
+        public event NotifyEventHandler Notify;
+        //public void TriggerEvents()
+        //{
+        //    Notify.Invoke("Event triggered!");
+        //}
+        public void NotifySubscribers(string message)
+        {
+            Notify?.Invoke(message); // Null-conditional operator to avoid null reference exception
+        }
+    }
+}
---
+++ CSharpDemos/25Demo_GenericDelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _25Demo_GenericDelegate
+{
+    public delegate T MyDelegate<T>();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SampleClass sample = new SampleClass();
+            //MyDelegate<SampleClass> del = new MyDelegate<SampleClass>(sample.SampleMethod);
+        }
+    }
+    public class SampleClass<T>
+    {
+        public void SampleMethod()
+        {
+            Console.WriteLine("Sample Method");
+        }
+    }
+}
